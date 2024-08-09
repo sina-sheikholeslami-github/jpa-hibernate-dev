dc9a0cc50fcfe6dac7996b2825434e1f056321a0
sinasheikholeslami <sina@sinasheikholeslami.com>
2024-08-08 17:23:47 -0700

    initial commit

---
+++ src/main/java/com/sinasheikholeslami/jpa/hibernate/jpa_hibernate_dev/JpaHibernateDevApplication.java
@@ -0,0 +1,13 @@
+package com.sinasheikholeslami.jpa.hibernate.jpa_hibernate_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaHibernateDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaHibernateDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sinasheikholeslami/jpa/hibernate/jpa_hibernate_dev/JpaHibernateDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinasheikholeslami.jpa.hibernate.jpa_hibernate_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaHibernateDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
